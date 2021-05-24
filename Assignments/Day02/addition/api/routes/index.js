ee460b459cf46539f1b804873be7f3b15415ad8d
misgan asmamaw <hello@misgancode>
2021-05-24 16:25:29 -0500

    All my labs

--- Assignments/Day02/addition/api/routes/index.js
+++
@@ -1,6 +0,0 @@
-let express= require("express");
-let router= express.Router();
-let additionController = require("../controllers/additionController.js");
-router.route("/add/:firstNumber").get(additionController.getSummation);
-module.exports = router;
- 

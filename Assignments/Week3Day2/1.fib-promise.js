8b263ed9bbc3f49a117a0f72562c4ec17e7f6767
misgan asmamaw <hello@misgancode>
2021-06-02 20:42:56 -0500

    Week3 Day2

---
+++ Assignments/Week3Day2/1.fib-promise.js
@@ -0,0 +1,27 @@
+const fib = function (num) {
+    if (num <= 2) {
+        return 1;
+    } else {
+        return fib(num - 1) + fib(num - 2);
+    }
+};
+
+const fibPromise = function (number) {
+    return new Promise((resolve, reject) => {
+        if (number > 0) {
+            resolve(fib(number));
+        } else {
+            reject("Number cannot be less than zero");
+        }
+    });
+}
+
+console.log("Start");
+
+fibPromise(30).then(result => {
+    console.log(result);
+}).catch(error => {
+    console.log(error);
+});
+
+console.log("End")
